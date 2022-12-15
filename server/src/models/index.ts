138a5d81482e75b70a5d4ac6dce9126cba1a5055
NahinChowdhury <46066736+NahinChowdhury@users.noreply.github.com>
2022-12-14 21:37:58 -0500

    cleanup and readme for models

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -5,8 +5,6 @@
 // Tutorial taken from
 // https://www.youtube.com/watch?v=7D0x79lLevs&ab_channel=WyattFleming
 
-
-import { Sequelize } from "sequelize-typescript";
 import { Client } from 'pg';
 
 
@@ -18,49 +16,25 @@ export const client = new Client({
     database: "nahintest",
 });
 
-// const sequelize = new Sequelize({
-//     host: "localhost",
-//     database: "Comedy Bar DB",
-//     port: 5432,
-//     username: "postgres",
-//     password: "password",
-//     dialect: 'postgres'
-// });
 
 export const initDB = async () => {
-    // await sequelize.authenticate();
-    // await sequelize.sync({ alter: true });
 
+    // Connect to the "nahintest" database
+    client.connect((err) => {
+        if (err) {
+            console.error('Error connecting to the database:', err.stack);
+            return;
+        }
+        
+        console.log('Connected to the database.');
+    });
 
-    // client.connect();
-
-    // client.query("SELECT * FROM User", (err, res)=> {
+    // client.query(`SELECT * FROM public."User"`, (err, res)=> {
     //     if(!err){
     //         console.log(res.rows);
     //     }else{
     //         console.log(err.message);
     //     }
     // })
-
-    // Connect to the "nahintest" database
-    client.connect((err) => {
-        if (err) {
-        console.error('Error connecting to the database:', err.stack);
-        return;
-        }
     
-        console.log('Connected to the database.');
-    });
-    
-    // Query the data from the "User" table
-    // client.query('SELECT * FROM "User"', (err, res) => {
-    //     if (err) {
-    //     console.error('Error querying the database:', err.stack);
-    //     return;
-    //     }
-    
-    //     console.log('Data from the "User" table:', res.rows);
-    // });
-
-    console.log("All good")
 };
