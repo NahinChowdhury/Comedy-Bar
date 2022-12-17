f5fc923134f9d7de5f525f1e89457f7522ce11a3
NahinChowdhury <46066736+NahinChowdhury@users.noreply.github.com>
2022-12-16 22:30:55 -0500

    created notfound and profile page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,43 @@
 import { Route, Routes as Switch, BrowserRouter as Router } from "react-router-dom";
-import { Header, Footer } from  './components/Index';
+import { PrivateRoute } from './components/PrivateRoute';
+import { Header, Footer } from './components/Index';
 import { Main } from './main/Main';
 import { Login } from './main/Login';
 import { Signup } from './main/Signup';
 import './App.css';
+import { Profile } from "./main/Profile";
+import { NotFound } from "./others/NotFound";
 
 function App() {
   return (
     <>
-		<Header />
 		
 		<Router>
+		<Header />
+		
 			<Switch>
 
-				<Route path="/" element={<Main />} />
-				<Route path="/hello" element={<>Hello this is hello page</>}/>
+				<Route path="/" element={ <PrivateRoute />  }>
+					<Route path="/" element={<Main />}/>
+				</Route>
+				<Route path="/" element={ <PrivateRoute />  }>
+					<Route path="/hello" element={<>Hello this is hello page</>}/>
+				</Route>
+				<Route path="/profile" element={ <PrivateRoute />  }>
+					<Route path="/profile" element={ <Profile /> } />
+				</Route>
+				<Route path="/" element={ <PrivateRoute />  }>
+					<Route path="/signup" element={ <Signup /> } />
+				</Route>
 				<Route path="/login" element={<Login />}/>
-				<Route path="/signup" element={<Signup />}/>
-				<Route path="*" element={<>404 page not found</>} />
+
+				<Route path="*" element={<NotFound />} />
 
 			</Switch>
+		
+		<Footer />
 		</Router>
 
-		<Footer />
     </>
   );
 }
---
+++ client/src/main/Profile.tsx
@@ -0,0 +1,49 @@
+import axios from "axios";
+import React, {FunctionComponent, useState, useEffect} from "react";
+import { Link } from "react-router-dom";
+
+
+export const Profile:FunctionComponent = () => {
+
+    const [username, setUsername] = useState<string>("");
+    const [firstname, setFirstname] = useState<string>("");
+    const [lastname, setLastname] = useState<string>("");
+
+
+
+    useEffect(() => {
+        axios.get("/api/user/profile")
+        .then(res => {
+            const {username, firstname, lastname} = res.data;
+
+            setUsername(username);
+            setFirstname(firstname);
+            setLastname(lastname);
+            // window.location.pathname = '/';
+        })
+        .catch(e => {
+            const error = e.response.data;
+            console.log(e);
+            switch(e.response.status){
+                case 401:
+                    console.log("error 401")
+                    window.location.pathname = '/login';
+                    break;
+                default:
+                    alert(`${error.message}. CODE: ${error.code}`);
+            }
+
+
+        })
+    },[])
+
+    return ( 
+        <div className="signup">
+            
+            Hi {username}, Your first name is {`[${firstname}]`} and last name is {`[${lastname}]`}
+            <div>Have an account? Log In!</div>
+            <Link to="/login">Go to Log In</Link>
+        </div>
+        
+    )
+}
