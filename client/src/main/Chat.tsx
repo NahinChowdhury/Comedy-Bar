fea12268d5890a26098158ed7e7fc07b9a8e4bfc
NahinChowdhury <46066736+NahinChowdhury@users.noreply.github.com>
2022-12-27 16:35:37 -0500

    Users can now see old chat messages

--- client/src/main/Chat.tsx
+++ client/src/main/Chat.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { FunctionComponent, useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import io from 'socket.io-client';
@@ -20,52 +21,163 @@ export const Chat:FunctionComponent = () => {
     const [socketName, setSocketName] = useState<string>(socket.id);
     const [room, setRoom] = useState<string>(`${Math.round(Math.random())%2}`);
 
+    const [hasAccess, setHasAccess] = useState<boolean>(false);
+
     const {chatId} = useParams();
 
-    useEffect(() => {
-
-        setRoom(chatId || ""); // should make sure chatId is valid before requesting to join
-        // console.log("sent join req")
-        socket.emit('join_room', {room: chatId, username});
-        setSocketName(socket.id);
-    }, [socket])
 
     useEffect(() => {
+        // send request to backend to make sure current user has access to the chat
+        // if not, turn off all socket functionalities
 
-        // might need to fetch all the chat messages before listening to socket
+        axios.get(`/api/user/chat/${chatId}/hasAccess`)
+            .then(res => {
+                const { hasAccess } = res.data;
+                
+                setHasAccess(hasAccess);
+            })
+            .catch(e => {
 
-        // Listen for a "receive_message" event from the server
-        socket.on('receive_message', (data) => {
-            // console.log(`Got message: ${data.message} by ${data.username}`)
-            // expecting data of format {room, details, sender, updatedAt}
-            setMessages([...messages, 
-                {
-                    sender: data.sender,
-                    details: data.details,
-                    read: false,
-                    updatedAt: data.updatedAt
+                const error = e.response.data;
+                console.log(e);
+                console.log(error);
+
+
+                switch(e.response.status){
+                    case 401:
+                        console.log("error 401")
+                        break;
+                    default:
+                        alert(`${error.message}. CODE: ${error.code}`);
                 }
-            ]);
-        });
-        setSocketName(socket.id)
+            })
+    }, [chatId])
 
-    }, [socket, messages]);      
 
-    const convertToAMPM = (date: Date) => {
-        return `${date.toLocaleTimeString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true })} ${date.toLocaleString('default', { month: 'long' })} ${date.getDate()}, ${date.getFullYear()}`
+    useEffect(() => {
+        
+        if(hasAccess){
+            if(chatId === undefined){
+                setHasAccess(false);
+            }
+
+            // reuqest to get all the chat history.
+            // once you fetch all the data, then try to join the room
+            requestData();
+        }
+    }, [socket, chatId, hasAccess]);
+
+    useEffect(() => {
+
+        if(hasAccess){
+            // might need to fetch all the chat messages before listening to socket
+
+            // Listen for a "receive_message" event from the server
+            socket.on('receive_message', (data) => {
+                // console.log(`Got message: ${data.message} by ${data.username}`)
+                // expecting data of format {room, details, sender, updatedAt}
+                setMessages([...messages, 
+                    {
+                        sender: data.sender,
+                        details: data.details,
+                        read: false,
+                        updatedAt: data.updatedAt
+                    }
+                ]);
+            });
+            setSocketName(socket.id)
+        }
+
+    }, [socket, messages, hasAccess]);      
+
+    
+    const requestData = async () => {
+        await axios.get(`/api/user/chat/${chatId}/getMessages`)
+            .then(res => {
+                const {messages} = res.data;
+
+                if(messages.length === 0){
+                    setMessages([]);
+                    alert(`This chat has no previous messages.`);
+                }else{
+
+                    setMessages(() => {
+                        return messages.map( (message:ChatMessage) => {
+                            return {
+                                sender: message.sender,
+                                details: message.details,
+                                read: message.read,
+                                updatedAt: message.updatedAt
+                            }
+                        })
+                    });
+                }
+
+                setRoom(chatId || "");
+                joinRoom();
+                setSocketName(socket.id);
+            })
+            .catch(e => {
+
+                const error = e.response.data;
+                console.log(e);
+                console.log(error);
+
+
+                switch(e.response.status){
+                    case 401:
+                        console.log("error 401")
+                        break;
+                    default:
+                        alert(`${error.message}. CODE: ${error.code}`);
+                }
+            })
     }
 
+    const joinRoom = () => {
+        // console.log("sent join req")
+        socket.emit('join_room', {room: chatId, username});
+    }
     // need a useEffect to make sure the user accessing this page has permission to access this page
+    
+    const handleSendMessage = async () => {
+        if(hasAccess){
+            
+            const messageToSend = message;
 
-    const handleSendMessage = () => {
-        // Send a "send_message" event to the server
-        socket.emit('send_message', {room, details: message, sender: username, updatedAt: convertToAMPM(new Date())});
-        setMessage('');
+            await axios.post(`/api/user/chat/${chatId}/createMessage`, {details: messageToSend})
+                .then(res => {
+                    
+                    // message has been saved in the database
+                    
+                    // Send a "send_message" event to the server
+                    socket.emit('send_message', {room, details: messageToSend, sender: username, updatedAt: convertToAMPM(new Date())});
+                    setMessage('');
+                })
+                .catch(e => {
 
-        // need to send a post req to backend to save the message sent
+                    const error = e.response.data;
+                    console.log(e);
+                    console.log(error);
+
+
+                    switch(e.response.status){
+                        case 401:
+                            console.log("error 401")
+                            break;
+                        default:
+                            alert(`${error.message}. CODE: ${error.code}`);
+                    }
+                })
+        }
+    };
+    
+    const convertToAMPM = (date: Date) => {
+        return `${date.toLocaleTimeString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true })} ${date.toLocaleString('default', { month: 'long' })} ${date.getDate()}, ${date.getFullYear()}`
     };
 
     return (
+        hasAccess ?
         <div>
             <div>User: {username}</div>
             <div>Socket Name: {socketName}</div>
@@ -75,12 +187,15 @@ export const Chat:FunctionComponent = () => {
                 value={message}
                 onChange={(e) => setMessage(e.target.value)}
             />
-            <button onClick={handleSendMessage}>Send</button>
+            <button onClick={ async () => {
+                await handleSendMessage();
+            }}>Send</button>
             <ul>
                 {messages.map((m, i) => (
                     <li key={i}>{`${m.sender}: ${m.details} at ${m.updatedAt}`}</li>
                 ))}
             </ul>
-        </div>
+        </div>:
+        <>User does not have access to this page. Please try to log in and try again</>
     );
 }
--- server/src/models/Chat.ts
+++ server/src/models/Chat.ts
@@ -113,8 +113,114 @@ export class ChatModel implements ChatMessageInterface, ChatRoomInterface {
             })
         }
 
-        const query = `Select "ROOM_ID" FROM public."SingleChatRoom" s WHERE s."MEMBER_ONE" IN ($1, $2) AND s."MEMBER_TWO" IN ($3, $4) AND s."MEMBER_ONE" <> s."MEMBER_TWO";`
-        const params = [memberOne, memberTwo, memberOne, memberTwo];
+        const query = `Select "ROOM_ID" FROM public."SingleChatRoom" s WHERE s."MEMBER_ONE" IN ($1, $2) AND s."MEMBER_TWO" IN ($1, $2) AND s."MEMBER_ONE" <> s."MEMBER_TWO";`
+        const params = [memberOne, memberTwo];
+
+        return new Promise((resolve, reject) => {
+            client.query(query, params)
+                .then(res => {
+                    const data = res.rows;
+                    console.log("data")
+                    console.log(data)
+                    if(data.length > 0){
+                        resolve( new ChatModel(data[0]));
+                    }else{
+                        resolve(null);
+                    }
+                })
+                .catch(err => reject(err));
+        })
+    }
+    
+    static async checkUserHasAccess(memberOne: string, roomId: string): Promise<ChatRoomInterface | null> {
+
+        // memberOne is the user themselves
+
+        // making sure user does exist before fetching all other users
+        const memberOneExists= await LoginModel.findUser(memberOne);
+        if(memberOneExists === null){
+            return new Promise<ChatRoomInterface | null>((resolve, reject) => {
+                reject(
+                    {
+                        message: "Please log in and try again.",
+                        code:"CM003"
+                    })
+            })
+        }
+
+        const query = `Select "ROOM_ID" FROM public."SingleChatRoom" s WHERE (s."MEMBER_ONE" = $1 OR s."MEMBER_TWO" =$1) AND "ROOM_ID" = $2;`
+        const params = [memberOne, roomId];
+
+        return new Promise((resolve, reject) => {
+            client.query(query, params)
+                .then(res => {
+                    const data = res.rows;
+                    console.log("data")
+                    console.log(data)
+                    if(data.length > 0){
+                        resolve( new ChatModel(data[0]));
+                    }else{
+                        resolve(null);
+                    }
+                })
+                .catch(err => reject(err));
+        })
+    }
+
+    static async getMessages(memberOne: string, roomId: string): Promise<ChatMessageInterface[]> {
+
+        // memberOne is the user themselves
+
+        // making sure user does exist before fetching all other users
+        const memberOneExists= await LoginModel.findUser(memberOne);
+        if(memberOneExists === null){
+            return new Promise<ChatMessageInterface[]>((resolve, reject) => {
+                reject(
+                    {
+                        message: "Please log in and try again.",
+                        code:"CM005"
+                    })
+            })
+        }
+
+        const query = `Select * FROM public."SingleChatMessages" s WHERE s."ROOM_ID" = $1;`
+        const params = [roomId];
+
+        return new Promise((resolve, reject) => {
+            client.query(query, params)
+                .then(res => {
+                    const data = res.rows;
+                    console.log("data")
+                    console.log(data)
+                    resolve(
+                        data.map( d=> {
+                            return new ChatModel(d);
+                        })
+                    );
+                })
+                .catch(err => reject(err));
+        })
+    }
+
+    static async createMessage(memberOne: string, roomId: string, details: string): Promise<ChatMessageInterface | null> {
+
+        // memberOne is the user themselves
+
+        // making sure user has permission to create the message
+        // do not need to check if user exists because this.checkUserHasAccess already checks it once
+        const userHasAccess = await this.checkUserHasAccess(memberOne, roomId);
+        if(userHasAccess === null) {
+            return new Promise<ChatMessageInterface | null>((resolve, reject) => {
+                reject(
+                    {
+                        message: "User does not have permissions to send messages to this chat.",
+                        code:"CM004"
+                    })
+            })
+        }
+
+        const query = `INSERT INTO public."SingleChatMessages" ("ROOM_ID", "SENDER", "DETAILS") VALUES ($1, $2, $3) returning *;`
+        const params = [roomId, memberOne, details];
 
         return new Promise((resolve, reject) => {
             client.query(query, params)
